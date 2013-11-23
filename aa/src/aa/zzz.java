61fca3d5a4bb5ddb4e6bb5fbf72544f87ad5e078
zoubert <zouberthanem@gmail.com>
2013-11-24 00:35:58 +0100

    Version 1.1 **Listes des visiteurs**

--- aa/src/aa/zzz.java
+++ aa/src/aa/zzz.java
@@ -15,6 +15,34 @@ public class zzz extends JPanel {
 	 * Constructeur
 	 */
 	public zzz(){
+		/*
 		
+		String[] entetes = { "Id", "Nom", "Prenom"};
+
+		//                              [Lignes]       -    [Colones]
+		this.donnees = new Object[Nombre de visiteur(s)][nombre d'entete(s)];
+		
+		this.tableau = new JTable(donnees, entetes);
+		
+		this.scroll = new JScrollPane(tableau);
+		this.scroll.setPreferredSize(new Dimension(450, 200));
+	
+				ResultSet rs = Modele.getLesVisiteurs();
+		 try{
+				while(rs.next()){
+				
+					String id = rs.getString("id");
+					String nomVisiteur = rs.getString("nom");
+					String prenomVisiteur = rs.getString("prenom");
+					
+					this.donnees[i][0] = id
+					this.donnees[i][1] = nomVisiteur
+					this.donnees[i][2] = prenomVisiteur
+				}
+			 }
+				catch(SQLException e){
+					System.out.println(e);
+				}
+		*/
 	}
 }
