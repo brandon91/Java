257ee7a8d8d7368d6b141c2ef2e6110ac6dcc4b4
hzoub <zouberthanem@gmail.com>
2016-08-03 09:36:05 +0200

    Update zzz.java

--- aa/src/aa/zzz.java
+++ aa/src/aa/zzz.java
@@ -32,10 +32,11 @@ public class zzz extends JPanel {
 		this.scroll.setPreferredSize(new Dimension(450, 200));
 		
 		/**
-		 * Pour que cette boucle fonctionne il te faut tout d'abord une classe Visiteurs que tu vas créer dans le package Modele qui aura 
-		 * comme variable (id,nom,prenom) et qui prendra en parametre ces 3 variables dans son constructeur et une fonction 
-		 * ( function Arraylist<Visiteurs> getLesVisteurs(); ) qui sera créée dans la classe modele.java qui va renvoyer tous les visiteurs qui sont dans la BDD sous forme d'un Arraylist.
-		 * Ensuite il te faut dans la classe Visiteur pour chaque variable l'accesseur get() uniquement l'accesseur get() ,tu n'auras pas besoin du set() ;)
+		 * Pour que cette boucle fonctionne il faut tout d'abord créer une classe Visiteur que tu vas créer dans le package Modele qui aura 
+		 * comme variable (id,nom,prenom) et qui prendra en parametre ces 3 variables dans son constructeur et créer une fonction 
+		 * ( function Arraylist<Visiteur> lesVisiteurs(){} ) dans la classe Modele.java qui va renvoyer tous les visiteurs qui sont dans la BDD sous forme d'un Arraylist.
+		 * Ensuite il te faut dans la classe Visiteur pour chaque variable l'accesseur get().
+		 * getId(), getNom(), getPrenom(), les accesseurs set() tu en auras pas besoin pour cette exemple.
 		*/
 		
 		for (int i = 0; i < Modele.lesVisiteurs().size(); i++) {
